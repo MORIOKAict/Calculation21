2c132177b8407a66058c9a4c5aff37879389b82a
MORIOKAict <aokimorioka@yahoo.co.jp>
2025-06-13 16:38:46 +0900

    printAddとprintSub追加

--- Calculation/src/Calc.java
+++ Calculation/src/Calc.java
@@ -7,7 +7,19 @@ public class Calc {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		printAdd(2,3);
+		printSub(7,4);
+	
+	}
+	public static void printAdd (int arg1,int arg2) {
+		int sum = arg1 + arg2;
+		String result = arg1+"+"+arg2+"="+sum;
+		System.out.println(result);
+	}
+	public static void printSub(int arg1,int arg2) {
+		int difference  = arg1 - arg2;
+		String result = arg1 + "-" + arg2 + "=" + difference ;
+		System.out.println(result);
 	}
 
 }
